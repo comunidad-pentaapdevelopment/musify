fd21428ab00b1031971a6ea8a02d26e122e15830
Matias Ruiz Moreno <matiasrm14@gmail.com>
2017-03-02 09:44:47 -0300

    Agregamos la carpeta services con los servicios del usuario

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,18 +1,121 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/user.service';
 import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  providers: [UserService]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
   public title = 'MUSIFY!';
-  public user = User;
+  public user : User;
+  public user_register : User;
   public identity; // guardamos los datos del usuario que se ha logueado
   public token;
+  public errorMessage;
+  public alertRegister;
 
-  constructor(){
+  constructor(
+  	private _userService:UserService
+  	){
   	this.user = new User('','','','','','ROLE_USER','');
+  	this.user_register = new User('','','','','','ROLE_USER','');
+  }
+
+  ngOnInit(){
+  	this.identity = this._userService.getIdentity();
+  	this.token = this._userService.getToken();
+
+  	console.log(this.identity);
+  	console.log(this.token);
+  }
+
+  public onSubmit(){
+  	console.log(this.user);
+
+  	// Conseguir los datos del usuario identificado 
+  	this._userService.signup(this.user).subscribe(
+  		response => {
+  			let identity = response.user;
+  			this.identity = identity;
+
+  			if(!this.identity._id){
+  				alert("El usuario no esta correctamente identificado");
+  			}else{
+  				// Crear elemento en el localstorage para tener al usuario en sesion
+  				localStorage.setItem('identity', JSON.stringify(identity));
+  				// Conseguir el token para enviarselo a cada peticion http
+  				this._userService.signup(this.user, 'true').subscribe(
+			  		response => {
+			  			let token = response.token;
+			  			this.token = token;
+
+			  			if(this.token.length <= 0){
+			  				alert("El token no se ha generado correctamente");
+			  			}else{
+			  				// Crear elemento en el localstorage para tener el token disponible
+			  				localStorage.setItem('token', token);
+
+			  				this.user = new User('','','','','','ROLE_USER','');
+			  			}
+			  		},
+			  		error => {
+			  			var errorMessage = <any>error;
+
+			  			if(errorMessage != null){
+			  				var body = JSON.parse(error._body);
+			  				this.errorMessage = body.message;
+			  			}
+			  		}
+			  	);
+  			}
+  		},
+  		error => {
+  			var errorMessage = <any>error;
+
+  			if(errorMessage != null){
+  				var body = JSON.parse(error._body);
+  				this.errorMessage = body.message;
+  			}
+  		}
+  	);
+  }
+
+  logout(){
+  	//Eliminar todo lo que hay en el localStorage al momento de cerrar sesion
+  	localStorage.removeItem('identity');
+  	localStorage.removeItem('token');
+  	localStorage.clear();
+  	this.identity = null;
+  	this.token = null;
+  }
+
+
+  onSubmitRegister(){
+  	console.log(this.user_register);
+
+  	this._userService.register(this.user_register).subscribe(
+  		response => {
+  			let user = response.user;
+  			this.user_register = user;
+
+  			if(!user._id){
+  				this.alertRegister = 'Error al registrarse';
+  			}else{
+  				this.alertRegister = 'El registro se ha realizado correctamente, identificate con '+this.user_register.email;
+  				this.user_register = new User('','','','','','ROLE_USER','');
+  			}
+  		},
+  		error => {
+  			var errorMessage = <any>error;
+
+  			if(errorMessage != null){
+  				var body = JSON.parse(error._body);
+  				this.alertRegister = body.message;
+  			}
+  		}
+  	);
   }
 }
